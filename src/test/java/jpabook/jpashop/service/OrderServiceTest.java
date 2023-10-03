a1c88fd290da23cc62fda503e3d554afcf8e182f
여름 <pjw7200@ajou.ac.kr>
2023-10-03 15:12:16 +0900

    ADD: OrderServiceTest

---
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,103 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.exception.NotEnoughStockException;
+import jpabook.jpashop.repository.OrderRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class OrderServiceTest {
+
+    @Autowired
+    EntityManager em;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void order() {
+        //given
+        Member member = createMember("name1");
+        Book book = createBook("book1", 10000, 10);
+
+        int orderCount = 2;
+
+        //when
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals(OrderStatus.ORDER, getOrder.getStatus());
+        assertEquals(1, getOrder.getOrderItems().size());
+        assertEquals(10000 * 2, getOrder.getTotalPrice());
+        assertEquals(8, book.getStockQuantity());
+    }
+
+    @Test
+    void cancelOrder() {
+        //given
+        Member member = createMember("name1");
+        Book book = createBook("book1", 10000, 10);
+
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        //when
+        orderService.cancelOrder(orderId);
+
+        //then
+        Order order = orderRepository.findOne(orderId);
+
+        assertEquals(OrderStatus.CANCEL, order.getStatus());
+        assertEquals(10, book.getStockQuantity());
+    }
+
+    @Test
+    public void overflow() throws Exception{
+        //given
+        Member member = createMember("name1");
+        Book book = createBook("book1", 10000, 10);
+
+        int orderCount = 11;
+
+        //when
+        try {
+            orderService.order(member.getId(), book.getId(), orderCount);
+        }catch (NotEnoughStockException e){
+            return;
+        }
+        //then
+        fail();
+    }
+
+    private Book createBook(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
+    }
+
+    private Member createMember(String name) {
+        Member member = new Member();
+        member.setName(name);
+        member.setAddress(new Address("seoul", "riverside", "123-123"));
+        em.persist(member);
+        return member;
+    }
+}
